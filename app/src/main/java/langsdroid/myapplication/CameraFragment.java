02b6b6bbb66cf21c86a64d196234c42e31007b3b
langsmith <langstonlmcs@gmail.com>
2016-10-21 16:25:42 -0700

    removed recyclerview work for later

--- app/src/main/java/langsdroid/myapplication/CameraFragment.java
+++ app/src/main/java/langsdroid/myapplication/CameraFragment.java
@@ -1,17 +1,17 @@
 package langsdroid.myapplication;
 
-import android.graphics.Camera;
+import android.hardware.Camera;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.util.Size;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import java.io.IOException;
-import java.util.List;
 
 /**
  * Created by LangstonSmith on 10/19/16.
@@ -19,18 +19,120 @@ import java.util.List;
 
 public class CameraFragment extends Fragment {
 
-    private Camera mCamera;
+    private SurfaceView preview = null;
+    private SurfaceHolder previewHolder = null;
+    private Camera camera = null;
+    private boolean inPreview = false;
+    private boolean cameraConfigured = false;
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.camera_fragment, container, false);
 
-        TextView tv = (TextView) v.findViewById(R.id.textview_camera_fragment);
-        tv.setText(getArguments().getString("msg"));
+        preview = (SurfaceView) v.findViewById(R.id.camera_surfaceview_preview);
+        previewHolder = preview.getHolder();
+        previewHolder.addCallback(surfaceCallback);
+        previewHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+
 
         return v;
     }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        camera = Camera.open();
+        startPreview();
+    }
+
+
+    @Override
+    public void onPause() {
+        if (inPreview) {
+            camera.stopPreview();
+        }
+
+        camera.release();
+        camera = null;
+        inPreview = false;
+
+        super.onPause();
+    }
+
+    private Camera.Size getBestPreviewSize(int width, int height,
+                                           Camera.Parameters parameters) {
+        Camera.Size result = null;
+
+        for (Camera.Size size : parameters.getSupportedPreviewSizes()) {
+            if (size.width <= width && size.height <= height) {
+                if (result == null) {
+                    result = size;
+                } else {
+                    int resultArea = result.width * result.height;
+                    int newArea = size.width * size.height;
+
+                    if (newArea > resultArea) {
+                        result = size;
+                    }
+                }
+            }
+        }
+
+        return (result);
+    }
+
+    private void initPreview(int width, int height) {
+        if (camera != null && previewHolder.getSurface() != null) {
+            try {
+                camera.setPreviewDisplay(previewHolder);
+            } catch (Throwable t) {
+                Log.e("CamFrag-surfaceCallback", "Exception in setPreviewDisplay()", t);
+
+            }
+
+            if (!cameraConfigured) {
+                Camera.Parameters parameters = camera.getParameters();
+                Camera.Size size = getBestPreviewSize(width, height,
+                        parameters);
+
+                if (size != null) {
+                    parameters.setPreviewSize(size.width, size.height);
+                    camera.setParameters(parameters);
+                    cameraConfigured = true;
+                }
+            }
+        }
+    }
+
+    private void startPreview() {
+        if (cameraConfigured && camera != null) {
+            camera.startPreview();
+            inPreview = true;
+        }
+    }
+
+    SurfaceHolder.Callback surfaceCallback = new SurfaceHolder.Callback() {
+        public void surfaceCreated(SurfaceHolder holder) {
+
+
+
+        }
+
+        public void surfaceChanged(SurfaceHolder holder,
+                                   int format, int width,
+                                   int height) {
+            initPreview(width, height);
+            startPreview();
+        }
+
+        public void surfaceDestroyed(SurfaceHolder holder) {
+
+
+        }
+    };
+
 
     public static CameraFragment newInstance(String text) {
 
@@ -42,11 +144,7 @@ public class CameraFragment extends Fragment {
 
         return f;
 
-
     }
 
-    @Override
-    public void onResume() {
-        super.onResume();
-    }
+
 }
--- app/src/main/java/langsdroid/myapplication/MainFragment.java
+++ app/src/main/java/langsdroid/myapplication/MainFragment.java
@@ -16,25 +16,6 @@ import android.widget.TextView;
 
 public class MainFragment extends Fragment {
 
-    private static final String TAG = "RecyclerViewFragment";
-    private static final String KEY_LAYOUT_MANAGER = "layoutManager";
-    private static final int SPAN_COUNT = 2;
-    private static final int DATASET_COUNT = 60;
-
-    private enum LayoutManagerType {
-        GRID_LAYOUT_MANAGER,
-        LINEAR_LAYOUT_MANAGER
-    }
-
-    protected LayoutManagerType mCurrentLayoutManagerType;
-
-    protected RecyclerView recyclerView;
-    protected RecyclerViewAdapter recyclerViewAdapter;
-    protected RecyclerView.LayoutManager layoutManager;
-    protected String[] dataset;
-
-
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.main_fragment, container, false);
@@ -42,22 +23,6 @@ public class MainFragment extends Fragment {
         TextView tv = (TextView) v.findViewById(R.id.textview_main_fragment);
         tv.setText(getArguments().getString("msg"));
 
-        layoutManager = new LinearLayoutManager(getActivity());
-
-        mCurrentLayoutManagerType = LayoutManagerType.LINEAR_LAYOUT_MANAGER;
-
-
-        if (savedInstanceState != null) {
-            // Restore saved layout manager type.
-            mCurrentLayoutManagerType = (LayoutManagerType) savedInstanceState
-                    .getSerializable(KEY_LAYOUT_MANAGER);
-        }
-
-        setRecyclerViewLayoutManager(mCurrentLayoutManagerType);
-
-        recyclerViewAdapter = new RecyclerViewAdapter(dataset);
-        // Set CustomAdapter as the adapter for RecyclerView.
-        recyclerView.setAdapter(recyclerViewAdapter);
 
         return v;
     }
@@ -73,55 +38,6 @@ public class MainFragment extends Fragment {
 
         return f;
     }
-
-    public void setRecyclerViewLayoutManager(LayoutManagerType layoutManagerType) {
-        int scrollPosition = 0;
-
-        if (recyclerView.getLayoutManager() != null) {
-            scrollPosition = ((LinearLayoutManager) recyclerView.getLayoutManager())
-                    .findFirstCompletelyVisibleItemPosition();
-        }
-
-        switch (layoutManagerType) {
-            case GRID_LAYOUT_MANAGER:
-                layoutManager = new GridLayoutManager(getActivity(), SPAN_COUNT);
-                mCurrentLayoutManagerType = LayoutManagerType.GRID_LAYOUT_MANAGER;
-                break;
-            case LINEAR_LAYOUT_MANAGER:
-                layoutManager = new LinearLayoutManager(getActivity());
-                mCurrentLayoutManagerType = LayoutManagerType.LINEAR_LAYOUT_MANAGER;
-                break;
-            default:
-                layoutManager = new LinearLayoutManager(getActivity());
-                mCurrentLayoutManagerType = LayoutManagerType.LINEAR_LAYOUT_MANAGER;
-        }
-
-        recyclerView.setLayoutManager(layoutManager);
-        recyclerView.scrollToPosition(scrollPosition);
-    }
-
-    @Override
-    public void onSaveInstanceState(Bundle savedInstanceState) {
-        // Save currently selected layout manager.
-        savedInstanceState.putSerializable(KEY_LAYOUT_MANAGER, mCurrentLayoutManagerType);
-        super.onSaveInstanceState(savedInstanceState);
-    }
-
-    /**
-     * Generates Strings for RecyclerView's adapter. This data would usually come
-     * from a local content provider or remote server.
-     */
-    private void initDataset() {
-        dataset = new String[DATASET_COUNT];
-        for (int i = 0; i < DATASET_COUNT; i++) {
-            dataset[i] = "This is element #" + i;
-        }
-    }
-
-
-
-
-
 
 
 }
