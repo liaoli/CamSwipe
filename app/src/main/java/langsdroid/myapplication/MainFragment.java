4579f60625d0e2a1651d2cbb24651f81407914da
langsmith <langstonlmcs@gmail.com>
2016-10-21 12:55:15 -0700

    added recycler view row

--- app/src/main/java/langsdroid/myapplication/MainFragment.java
+++ app/src/main/java/langsdroid/myapplication/MainFragment.java
@@ -2,6 +2,9 @@ package langsdroid.myapplication;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,6 +16,25 @@ import android.widget.TextView;
 
 public class MainFragment extends Fragment {
 
+    private static final String TAG = "RecyclerViewFragment";
+    private static final String KEY_LAYOUT_MANAGER = "layoutManager";
+    private static final int SPAN_COUNT = 2;
+    private static final int DATASET_COUNT = 60;
+
+    private enum LayoutManagerType {
+        GRID_LAYOUT_MANAGER,
+        LINEAR_LAYOUT_MANAGER
+    }
+
+    protected LayoutManagerType mCurrentLayoutManagerType;
+
+    protected RecyclerView recyclerView;
+    protected RecyclerViewAdapter recyclerViewAdapter;
+    protected RecyclerView.LayoutManager layoutManager;
+    protected String[] dataset;
+
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.main_fragment, container, false);
@@ -20,8 +42,26 @@ public class MainFragment extends Fragment {
         TextView tv = (TextView) v.findViewById(R.id.textview_main_fragment);
         tv.setText(getArguments().getString("msg"));
 
+        layoutManager = new LinearLayoutManager(getActivity());
+
+        mCurrentLayoutManagerType = LayoutManagerType.LINEAR_LAYOUT_MANAGER;
+
+
+        if (savedInstanceState != null) {
+            // Restore saved layout manager type.
+            mCurrentLayoutManagerType = (LayoutManagerType) savedInstanceState
+                    .getSerializable(KEY_LAYOUT_MANAGER);
+        }
+
+        setRecyclerViewLayoutManager(mCurrentLayoutManagerType);
+
+        recyclerViewAdapter = new RecyclerViewAdapter(dataset);
+        // Set CustomAdapter as the adapter for RecyclerView.
+        recyclerView.setAdapter(recyclerViewAdapter);
+
         return v;
     }
+
 
     public static MainFragment newInstance(String text) {
 
@@ -33,4 +73,55 @@ public class MainFragment extends Fragment {
 
         return f;
     }
+
+    public void setRecyclerViewLayoutManager(LayoutManagerType layoutManagerType) {
+        int scrollPosition = 0;
+
+        if (recyclerView.getLayoutManager() != null) {
+            scrollPosition = ((LinearLayoutManager) recyclerView.getLayoutManager())
+                    .findFirstCompletelyVisibleItemPosition();
+        }
+
+        switch (layoutManagerType) {
+            case GRID_LAYOUT_MANAGER:
+                layoutManager = new GridLayoutManager(getActivity(), SPAN_COUNT);
+                mCurrentLayoutManagerType = LayoutManagerType.GRID_LAYOUT_MANAGER;
+                break;
+            case LINEAR_LAYOUT_MANAGER:
+                layoutManager = new LinearLayoutManager(getActivity());
+                mCurrentLayoutManagerType = LayoutManagerType.LINEAR_LAYOUT_MANAGER;
+                break;
+            default:
+                layoutManager = new LinearLayoutManager(getActivity());
+                mCurrentLayoutManagerType = LayoutManagerType.LINEAR_LAYOUT_MANAGER;
+        }
+
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.scrollToPosition(scrollPosition);
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        // Save currently selected layout manager.
+        savedInstanceState.putSerializable(KEY_LAYOUT_MANAGER, mCurrentLayoutManagerType);
+        super.onSaveInstanceState(savedInstanceState);
+    }
+
+    /**
+     * Generates Strings for RecyclerView's adapter. This data would usually come
+     * from a local content provider or remote server.
+     */
+    private void initDataset() {
+        dataset = new String[DATASET_COUNT];
+        for (int i = 0; i < DATASET_COUNT; i++) {
+            dataset[i] = "This is element #" + i;
+        }
+    }
+
+
+
+
+
+
+
 }
